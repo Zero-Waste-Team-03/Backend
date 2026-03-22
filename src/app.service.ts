c7a1a00ba0a4dd499dabeb88a059c97ed38240b3
Mouloud <m.hasrane@esi-sba.dz>
2026-03-22 06:32:17 +0100

    test(logging):Add dummy log to test context

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { LogContext } from './monitoring/logger/decorators/log-context.decorator';
 
 @Injectable()
 export class AppService {
+  logger = new Logger(AppService.name);
   @LogContext()
   getHello(): string {
+    this.logger.log('Hello method called', {
+      test: 'test log context',
+      meta: { userId: 123 },
+    });
     return 'Hello to core app';
   }
 }
