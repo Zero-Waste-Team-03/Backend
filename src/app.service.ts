f39beead08ef524993789626f35cc654b8bfbd67
Mouloud <m.hasrane@esi-sba.dz>
2026-03-22 06:14:46 +0100

    feat(observabilty): Added new Log-context decorator

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { LogContext } from './monitoring/logger/decorators/log-context.decorator';
 
 @Injectable()
 export class AppService {
+  @LogContext()
   getHello(): string {
     return 'Hello to core app';
   }
