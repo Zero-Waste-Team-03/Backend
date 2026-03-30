d060b1039dbd71208d3740314d18b718ea33b92b
Mouloud <m.hasrane@esi-sba.dz>
2026-03-30 13:09:54 +0100

    feat(attachement): Define Attachement type

---
+++ src/common/modules/attachment/graphql/attachement.type.ts
@@ -0,0 +1,39 @@
+import { Field, registerEnumType } from '@nestjs/graphql';
+import {
+  UploadStatus,
+  UploadStatusValues,
+} from '../entities/attachment.entity';
+
+registerEnumType(() => UploadStatusValues, { name: 'UploadStatusValues' });
+export class AttachementType {
+  @Field(() => String, { description: 'Attachment unique identifier' })
+  id: string;
+
+  @Field(() => String, { description: 'URL of the attachment' })
+  url: string;
+  @Field(() => String, { description: 'Original file name of the attachment' })
+  fileName: string;
+  @Field(() => String, { description: 'MIME type of the attachment' })
+  fileType: string;
+  @Field(() => Number, { description: 'Size of the attachment in bytes' })
+  fileSize: number;
+
+  @Field(() => UploadStatusValues, {
+    description: 'Current upload status of the attachment',
+  })
+  uploadStatus: UploadStatus;
+  @Field(() => String, {
+    description: 'ID of the user who uploaded the attachment',
+  })
+  uploadedById: string;
+  @Field(() => String, {
+    description: 'ID of the background job processing the upload',
+  })
+  jobId: string;
+
+  @Field(() => Date, { description: 'Date the attachment was created' })
+  createdAt: Date;
+
+  @Field(() => Date, { description: 'Date the attachment was last updated' })
+  updatedAt: Date;
+}
