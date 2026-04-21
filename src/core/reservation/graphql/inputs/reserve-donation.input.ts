dcdcdafc87c296a05e54e03872ea9a48a0ba669e
Mouloud <m.hasrane@esi-sba.dz>
2026-04-21 10:26:14 +0100

    fix(donations): revert contract

--- src/core/reservation/graphql/inputs/reserve-donation.input.ts
+++ src/core/reservation/graphql/inputs/reserve-donation.input.ts
@@ -1,17 +1,7 @@
 import { ArgsType, Field, ID, Int } from '@nestjs/graphql';
-import { IsInt, Min } from 'class-validator';
 
 @ArgsType()
 export class ReserveDonationInput {
   @Field(() => ID)
   donationId: string;
-
-  @Field(() => Int, {
-    nullable: true,
-    defaultValue: 1,
-    description: 'Number of units to reserve (defaults to 1)',
-  })
-  @IsInt()
-  @Min(1)
-  quantity?: number;
 }
