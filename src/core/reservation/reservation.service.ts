e179b9ca3c37c0915ed140099fec177b17610ad5
Mouloud <m.hasrane@esi-sba.dz>
2026-05-15 17:26:10 +0100

    fix(reservation): replace quantity with actual quantity value on notificaiton

--- src/core/reservation/reservation.service.ts
+++ src/core/reservation/reservation.service.ts
@@ -252,7 +252,7 @@ queryBuilder.andWhere('reservation.beneficiaryId = :userId', { userId }).orWhere
 
     await this.notificationsService.sendNotification(
       'Donation reserved',
-      `${beneficiary?.displayName ?? 'A beneficiary'} reserved qty of ${result.donationTitle}.`,
+      `${beneficiary?.displayName ?? 'A beneficiary'} reserved ${result.reservation.quantity} of ${result.donationTitle}.`,
       result.donorId,
       NOTIFICATION_TYPE.RESERVATION_ALERT,
       {
